cb3c84dbf14958be64ceb5bed8bcd6e9aa0dcac7
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-03-20 00:02:00 -0300

    Add rainbow drawing functionality

--- src/app.js
+++ src/app.js
@@ -15,6 +15,19 @@ function fillSketchContainer() {
   }
 }
 
+function activateRainbow() {
+  const cells = document.querySelectorAll('.cell');
+
+  for (let i = 0; i < cells.length; i += 1) {
+    cells[i].addEventListener('mouseover', () => {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      cells[i].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    });
+  }
+}
+
 function activateBlack() {
   const cells = document.querySelectorAll('.cell');
 
@@ -30,6 +43,9 @@ function main() {
 
   const darkBtn = document.querySelector('.dark-btn');
   darkBtn.addEventListener('click', activateBlack);
+
+  const rainbowBtn = document.querySelector('.rainbow-btn');
+  rainbowBtn.addEventListener('click', activateRainbow);
 }
 
 main();
